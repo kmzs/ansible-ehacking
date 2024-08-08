c881881e2572528ea6ff16a6fe10a1c51258b5cc
Christian Mainka <christian.mainka@rub.de>
2024-08-08 16:00:33 +0200

    fix: add safety check

--- ubuntu_to_ehacking.sh
+++ ubuntu_to_ehacking.sh
@@ -6,7 +6,13 @@ sudo apt update
 echo "### Installing ansible"
 sudo apt install -y ansible git
 echo "### Cloning Git Repository"
+if [ ! -d "ansible-ehacking" ]; then
+    echo "### Cloning Git Repository"
 git clone --depth 1 https://git.noc.ruhr-uni-bochum.de/CheariX/ansible-ehacking.git
-cd ansible-ehacking
+else
+    echo "### Git repository already exists, updating..."
+    cd ansible-ehacking
+    git pull origin master
+fi
 echo "### Deployment via ansible"
 ansible-playbook -c local -i localhost, provisioning/playbook.yml --skip-tags="postman"
