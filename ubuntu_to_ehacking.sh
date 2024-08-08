6372cda19b1ad699878243ae9617e7d18c6f600f
Christian Mainka <christian.mainka@rub.de>
2024-08-08 15:49:29 +0200

    feat: curl pipe installer

--- ubuntu_to_ehacking.sh
+++ ubuntu_to_ehacking.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
-# echo "### Updating Packages"
-# sudo apt update
-# echo "### Installing ansible"
-# sudo apt install -y ansible
+echo "### Updating Packages"
+sudo apt update
+echo "### Installing ansible"
+sudo apt install -y ansible
+echo "### Cloning Git Repository"
+git clone --depth 1 https://git.noc.ruhr-uni-bochum.de/CheariX/ansible-ehacking.git
+cd ansible-ehacking
 echo "### Deployment via ansible"
 ansible-playbook -c local -i localhost, provisioning/playbook.yml --skip-tags="postman"
