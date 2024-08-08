198247d38de3067b8e5eded82dd53c1861f23fdf
Christian Mainka <christian.mainka@rub.de>
2024-08-08 16:05:29 +0200

    fix: more checks

--- ubuntu_to_ehacking.sh
+++ ubuntu_to_ehacking.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-echo "### Updating Packages"
-sudo apt update
-echo "### Installing ansible"
-sudo apt install -y ansible git
-echo "### Cloning Git Repository"
+if ! command -v ansible-playbook &> /dev/null || ! command -v git &> /dev/null; then
+    echo "### Updating Packages"
+    sudo apt update
+    echo "### Installing ansible and git"
+    sudo apt install -y ansible git
+else
+    echo "### Ansible and git already installed, skipping installation..."
+fi
 if [ ! -d "ansible-ehacking" ]; then
     echo "### Cloning Git Repository"
-git clone --depth 1 https://git.noc.ruhr-uni-bochum.de/CheariX/ansible-ehacking.git
+    git clone --depth 1 https://git.noc.ruhr-uni-bochum.de/CheariX/ansible-ehacking.git
 else
     echo "### Git repository already exists, updating..."
     cd ansible-ehacking
-    git pull origin master
+    git pull
 fi
 echo "### Deployment via ansible"
-ansible-playbook -c local -i localhost, provisioning/playbook.yml --skip-tags="postman"
+sudo ansible-playbook -c local -i localhost, provisioning/playbook.yml --skip-tags="postman"
