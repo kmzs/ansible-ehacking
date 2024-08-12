61329ced2efaa87531cfccb08d2b5d1b09f65f1b
Christian Mainka <christian.mainka@rub.de>
2024-08-12 13:54:32 +0200

    add: password for student

--- print_passwords.py
+++ print_passwords.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from passlib.hash import sha512_crypt
 
-hash = sha512_crypt.using(rounds=5000).hash("toor")
-print(f"Passwordhash for 'toor': {hash}")
-hash = sha512_crypt.using(rounds=5000).hash("kali")
-print(f"Passwordhash for 'kali': {hash}")
+for pw in [ "toor", "kali", "student" ]:
+    hash = sha512_crypt.using(rounds=5000).hash("toor")
+    print(f"Passwordhash for '{pw}': {hash}")
