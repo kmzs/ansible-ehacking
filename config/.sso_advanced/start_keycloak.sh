237d8a00588e782763dfc9997a2c02f03aceacd0
Karsten Meyer zu Selhausen <karsten.meyerzuselhausen@hackmanit.de>
2025-09-25 13:49:26 +0200

    Updated start_keycloak.sh script

--- config/.sso_advanced/start_keycloak.sh
+++ config/.sso_advanced/start_keycloak.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+set -e
 
-docker-compose -f /home/user/.sso_advanced/docker-compose.yml down
-docker-compose -f /home/user/.sso_advanced/docker-compose.yml up -d
+# determine docker compose command
+if command -v docker &>/dev/null && docker --help | grep -q "compose"; then
+    DOCKER_COMPOSE="docker compose"
+else
+    DOCKER_COMPOSE="docker-compose"
+fi
+
+# load .env if present
+[ -f .env ] && set -o allexport && . .env && set +o allexport
+
+cd "$HOME/.sso_advanced"
+$DOCKER_COMPOSE down
+$DOCKER_COMPOSE pull
+docker system prune -f
+$DOCKER_COMPOSE up -d
